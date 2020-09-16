79cca18c66c6bd972681ef1f8d62c2e51da32c10
Ashen759 <sjx001005>
2020-09-16 17:15:00 +0800

    修改名称

---
+++ 毕业项目源码和数据库/山珍海味美食网源码/DelicaciesFood/Controllers/FoodsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DelicaciesFood.Model;
+using PagedList;
+
+namespace DelicaciesFood.Controllers
+{
+    public class FoodsController : Controller
+    {
+        FoodieWebsiteEntities db = new FoodieWebsiteEntities();
+        [Authorize]
+        // GET: Foods
+        public ActionResult Index(int? page = null)
+        {
+
+            //类别
+            var typeList = db.RecipeType.ToList();
+            ViewBag.typeList = typeList;
+
+            //菜品
+            var foodsList = db.Food.ToList();
+            ViewBag.foodsList = foodsList;
+
+
+
+            
+            return View();
+        }
+        public ActionResult Detail(int? id)
+        {
+            ViewBag.foods = db.Food.Find(id);
+            return View();
+        }
+
+    }
+}
---
+++ 毕业项目源码和数据库/山珍海味美食网源码/DelicaciesFood/Controllers/HealthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DelicaciesFood.Model;
+
+namespace DelicaciesFood.Controllers
+{
+    public class HealthController : Controller
+    {
+        FoodieWebsiteEntities db = new FoodieWebsiteEntities();
+        // GET: Health
+        [Authorize]
+        public ActionResult Index()
+        {
+            var dietList = db.DietNous.ToList();
+            ViewBag.dietList = dietList;
+
+            //月份
+            ViewBag.time = DateTime.Now.Month.ToString();
+
+            //判断当前月份并显示相对应的时蔬表
+            int month = int.Parse(DateTime.Now.Month.ToString());
+            List<Sfav> sfavs = db.Sfav.Where(p => p.MatureSeason == month).ToList();
+            ViewBag.sfavsList = sfavs;
+
+            return View();
+        }
+        public ActionResult DietNous(int? nous,int? num)
+        {
+            List<DietNous> dietnous = db.DietNous.Where(p => p.NousID == nous).ToList();
+            ViewBag.dietnous = dietnous;
+
+            List<Sfav>sfavs  = db.Sfav.Where(p => p.SfavID == num).ToList();
+            ViewBag.sfavs = sfavs;
+
+            return View();
+        }
+    }
+}
